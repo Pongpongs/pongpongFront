1d551f99cd6d8576a86374126a2dc7c9d5300430
yback1223 <yoyob1223@gmail.com>
2024-02-27 17:52:26 +0900

    #9 2FA 모달 로직 구현

--- app.js
+++ app.js
@@ -1,21 +1,145 @@
 const express = require('express');
-const path = require("path");
+const https = require('https');
+const querystring = require('querystring');
+const path = require('path');
 const app = express();
+const nodemailer = require('nodemailer');
 const port = 3000;
 
-// 정적 파일을 제공하는 설정
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'frontend')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve("frontend", "index.html"));
 });
 
-app.get('/oauth/securitycode', (req, res) => {
-	const securitycode = req.query.code;
-	console.log(req.query.code);
-	res.send(req.query.code);
-})
+app.get('/get/security', (req, res) => {
+	const code = req.query.code;
+	if (code) {
+		res.redirect(`http://localhost:3000?code=${code}`)
+	} else {
+		res.status(400).send("Code not given");
+	}
+});
+
+app.get('/game/*', (req, res) => {
+	res.sendFile(path.resolve("frontend", "index.html"));
+});
+
+app.post('/get/security', (req, res) => {
+    const code = req.body.code;
+    const postData = querystring.stringify({
+        grant_type: 'authorization_code',
+        client_id: 'u-s4t2ud-38e1085a57405a04552b3a4b255d1b9a1708d9a23637ac72693e9469b06a779f',
+        client_secret: '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',
+        code: code,
+        redirect_uri: 'http://localhost:3000/get/security'
+    });
+
+    const options = {
+        hostname: 'api.intra.42.fr',
+        path: '/oauth/token',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+    };
+
+    const request = https.request(options, response => {
+        let data = '';
+        response.on('data', chunk => {
+            data += chunk;
+        });
+        response.on('end', () => {
+            console.log('Token data:', data);
+            res.json(JSON.parse(data));
+        });
+    });
+
+    request.on('error', error => {
+        console.error('Error requesting access token:', error);
+        res.status(500).send('Failed to obtain access token');
+    });
+
+    request.write(postData);
+    request.end();
+});
+
+app.get('/get/userinfo', (req, res) => {
+    const access_token = req.headers.authorization;
+
+    
+    const getUserInfo = () => {
+        return new Promise((resolve, reject) => {
+            const options = {
+                hostname: 'api.intra.42.fr',
+                path: '/v2/me',
+                method: 'GET',
+                headers: {
+                    'Authorization': access_token
+                }
+            };
+
+            const request = https.request(options, response => {
+                let data = '';
+                response.on('data', chunk => {
+                    data += chunk;
+                });
+                response.on('end', () => {
+                    resolve(data); 
+                });
+            });
+
+            request.on('error', error => {
+                reject(error); 
+            });
+
+            request.end();
+        });
+    };
+
+    
+    const sendEmail = async (userEmail, verificationCode) => {
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'yoyob1223@gmail.com',
+                pass: 'jhdf nkre qcrr cbkm'
+            }
+        });
+
+        let mailOptions = {
+            from: 'yoyob1223@gmail.com',
+            to: userEmail,
+            subject: 'Pongpongs 2FA Code',
+            text: `Your Verification Code: ${verificationCode}`
+        };
+
+        try {
+            let info = await transporter.sendMail(mailOptions);
+            console.log('Email sent:', info.response);
+        } catch (error) {
+            console.error('Error sending email:', error);
+        }
+    };
+
+    
+    (async () => {
+        try {
+            const data = await getUserInfo();
+            const parsedData = JSON.parse(data); 
+            const userEmail = parsedData.email;
+
+            const verificationCode = Math.floor(100000 + Math.random() * 900000);
+            await sendEmail(userEmail, verificationCode); 
+            res.json({email: userEmail, code: verificationCode});
+        } catch (error) {
+            console.error('Error:', error);
+            res.status(500).send('An error occurred');
+        }
+    })();
+});
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,5 +1,128 @@
 import LoginModal from "./views/LoginModal.js";
+import FA2Modal from "./views/FA2Modal.js";
+import Local1on1 from "./views/Local1on1View.js";
+import Online1on1 from "./views/Online1on1View.js";
+import Online4 from "./views/Online4View.js";
+import OnlineTournament from "./views/OnlineTournamentView.js";
+import GameSelectView from "./views/GameSelectView.js";
+import GameEnterView from "./views/GameEnterView.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-    LoginModal.displayLoginModal();
-})
+document.addEventListener('DOMContentLoaded', async () => {
+    initializeRouting();
+    window.onpopstate = routePage; 
+   	await routePage(); 
+
+    let access_token = localStorage.getItem('access_token');
+	let access_token_expire_time = Number(localStorage.getItem('expire_time'));
+	let current_time = Math.floor(Date.now() / 1000);
+	if (access_token_expire_time <= current_time) localStorage.clear();
+    
+    if (!access_token || !localStorage.getItem('FA2_verified')) {
+        let security_code = getCodeFromUrl();
+
+        if (!security_code) {
+            LoginModal.displayLoginModal();
+        } else if (!access_token) {
+            await fetchAccessToken(security_code);
+            access_token = localStorage.getItem('access_token'); 
+            const emailAndCode = await getEmailAndCode(access_token);
+          	await FA2Modal.displayFA2Modal(emailAndCode, access_token); 
+        } else {
+            const emailAndCode = await getEmailAndCode(access_token);
+			await FA2Modal.displayFA2Modal(emailAndCode, access_token); 
+        }
+    }
+});
+
+function navigate(path) {
+    history.pushState(null, null, path);
+    routePage();
+}
+
+function initializeRouting() {
+    document.addEventListener('click', e => {
+        if (e.target.matches('[data-link]')) {
+            e.preventDefault();
+            navigate(e.target.href);
+        }
+    });
+}
+
+async function routePage() {
+    const routes = [
+        { path: "/", view: GameEnterView },
+        { path: "/game/select", view: GameSelectView },
+        { path: "/game/local/1on1", view: Local1on1 },
+        { path: "/game/online/1on1", view: Online1on1 },
+        { path: "/game/online/4", view: Online4 },
+        { path: "/game/online/tournament", view: OnlineTournament }
+    ];
+
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        };
+    });
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+
+    if (!match) {
+        match = { route: "/", view: GameEnterView }; 
+        history.pushState(null, null, "/");
+    }
+
+    const view = new match.route.view({ navigate });
+    document.querySelector("#app").innerHTML = await view.getHtml();
+
+	if (view.addEventListeners && typeof view.addEventListeners === 'function') {
+        view.addEventListeners();
+    }
+}
+
+
+function getCodeFromUrl() {
+    const security_code = new URLSearchParams(window.location.search).get('code');
+	if (security_code) {
+		history.replaceState(null, '', window.location.pathname);
+		routePage();
+	}
+    return security_code
+}
+
+async function getEmailAndCode(access_token) { 
+    try {
+        const response = await fetch('/get/userinfo', { 
+            method: 'get',
+            headers: {
+                'Authorization': 'Bearer ' + access_token
+            }
+        });
+        const data = await response.json(); 
+        return data; 
+    } catch (error) {
+        console.error('Error fetching user info', error);
+    }
+}
+
+async function fetchAccessToken(code) { 
+    try {
+        const response = await fetch('/get/security', { 
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ code: code })
+        });
+        const data = await response.json();
+
+        if (data.access_token) {
+            localStorage.setItem('access_token', data.access_token);
+			localStorage.setItem('expire_time', data.created_at + data.expires_in);
+        } else {
+            console.log('Failed to obtain access token.');
+        }
+    } catch (error) {
+        console.error('Error fetching access token:', error);
+    }
+}
